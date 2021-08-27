b9807a9b829ab199738d01ad5a96375251a9e166
shivraj2806 <shivjadhav2806@gmail.com>
2021-08-27 14:09:25 +0530

    Added: adding a value in linklist

---
+++ src/com/Bridgelabz/Main.java
@@ -0,0 +1,13 @@
+package com.Bridgelabz;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyLinkedLink addLinkList = new MyLinkedLink();
+        addLinkList.add(56);
+        addLinkList.add(30);
+        addLinkList.add(70);
+        addLinkList.display(addLinkList.head);
+
+    }
+}
--- src/com/Bridgelabz/MyLinkedLink.java
+++ src/com/Bridgelabz/MyLinkedLink.java
@@ -1,7 +1,52 @@
 package com.Bridgelabz;
+import java.util.*;
+class Node {
+        int data;
+        Node next;
 
-public class MyLinkedLink {
-    public static void main(String[] args) {
-        System.out.println("welcome to Linkedlist");
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-}
+
+    public class MyLinkedLink {
+        Node head = null;
+        Node tail;
+
+        public void add(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                return;
+            }
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+            tail = newNode;
+        }
+        public boolean emptyCheck() {
+            if (head == null) {
+                return true;
+
+            }
+            return false;
+        }
+        public void display(Node head) {
+            if (emptyCheck()) {
+                System.out.println("Empty Linkedlist");
+                return;
+            }
+
+            Node temp = head;
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+
+        }
+
+
+    }
